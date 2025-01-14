8238b22a1adaa5cf0b763acd01328375c5359b98
r0ddty <r0d@ukr.net>
2025-01-14 23:08:04 +0200

    cactus: replace build script with better one
    Signed-off-by: r0ddty <r0d@ukr.net>

--- build.sh
+++
@@ -1,5 +0,0 @@
-git clone https://gitlab.com/kalilinux/packages/gcc-arm-linux-gnueabihf-4-7
-export CROSS_COMPILE=$(pwd)/gcc-arm-linux-gnueabihf-4-7/bin/arm-linux-gnueabihf-
-export ARCH=arm && export SUBARCH=arm
-make cactus_defconfig O=out
-make -j$(nproc --all) O=out
