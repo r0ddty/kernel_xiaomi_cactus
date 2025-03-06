b801b82e931d672158a261fef8c9d2c820d81478
r0ddty <r0d@ukr.net>
2025-03-06 15:52:48 +0200

    Revert "cactus: use external sdcard as a data device"
    This reverts commit 1cf91ce976e52b6caec6601bcbe8a9060e7c9aab.

--- debian/kernel-info.mk
+++ debian/kernel-info.mk
@@ -11,7 +11,7 @@ VARIANT = android
 KERNEL_BASE_VERSION = 4.9-200
 
 # The kernel cmdline to use
-KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2 androidboot.selinux=permissive buildvariant=userdebug datapart=/dev/mmcblk1p1
+KERNEL_BOOTIMAGE_CMDLINE = bootopt=64S3,32N2,64N2 androidboot.selinux=permissive buildvariant=userdebug
 
 # Slug for the device vendor. This is going to be used in the KERNELRELASE
 # and package names.
